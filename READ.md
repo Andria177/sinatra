ef91137f62b6b26b69483c308f23a2132fba7817
Andria177 <andriarzk@gmail.com>
2020-04-28 00:57:57 +0300

    envoi 2

---
+++ READ.md
@@ -0,0 +1,6 @@
+Bonjour!
+Ceci est ma première application web sous sinatre. Je vous demanderai d'être indulgent SVP. Je suis sûr que cela va s'améliorer par la suite.
+
+Je ne suis arrivé aue jusqu'à 2.4.3, c'est à dire jusau'à la redirection vers l'accueil.
+
+Merci et une bonne semaine à vous!
---
+++ lib/controller.rb
@@ -0,0 +1,17 @@
+require 'gossip'
+
+class ApplicationController < Sinatra::Base
+  get '/' do
+    erb :index
+  end
+
+  get '/gossips/new/' do
+    erb :new_gossip
+  end
+
+  post '/gossips/new/' do
+    #puts "Ce programme ne fait rien pour le moment, on va donc afficher un message dans le terminal"
+    Gossip.new(params["gossip_author"],params["gossip_content"]).save
+    redirect '/'
+  end
+end
